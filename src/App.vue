1d8f4f85952cfa01c12c045315eae97a09592d67
冯陈诚 <523246656@qq.com>
2017-07-12 00:02:16 +0800

    搭建页面结构-sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
+  <el-row class="app-wp">
+    <el-col :xs="12" :sm="6" :md="6" :lg="6" class="side-bar">
+      <nav>
+        <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-menu-item index="1"><i class="el-icon-menu"></i>主页</el-menu-item>
+          <el-menu-item index="2"><i class="el-icon-setting"></i>文章</el-menu-item>
+          <el-menu-item index="3"><i class="el-icon-setting"></i>绘画集</el-menu-item>
+          <el-menu-item index="4"><i class="el-icon-setting"></i>Demo</el-menu-item>
+          <el-menu-item index="5"><i class="el-icon-setting"></i>About</el-menu-item>
+        </el-menu>
+      </nav>
+    </el-col>
+    <el-col :xs="12" :sm="18" :md="18" :lg="18" class="content-wp">
 
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      author: 'fengcc'
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
 <style>
-
+html, body, .app-wp{
+  height: 100%;
+}
+.side-bar{
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background-color: #ccc;
+}
+.side-bar nav{
+  width: 100%;
+  text-align: right;
+}
+.content-wp{
+  height: 100%;
+}
+.el-menu-item{
+  padding-right: 40px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import './assets/reset.css'
+// import router from './router/index'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = true
 
 /* eslint-disable no-new */
 new Vue({
- 
+  el: '#app',
+  template: '<app></app>',
+  components: {
+    App
+  }
 })
